a07556c3d21b7adb55829594e73e36cd189108f8
BoubouSW <gilliclement@gmail.com>
2022-02-01 15:59:46 +0100

    add matrice

---
+++ modules/matrice.py
@@ -0,0 +1,9 @@
+from random import randrange
+
+def random_int_list(n,bound):
+    tab = []
+    for i in range(0,bound):
+        tab.append(randrange(0,bound))
+    return tab
+
+print(random_int_list(5, 10))
