b64a09a09c4a332b897eaa996d06e3504d0fdd2f
BoubouSW <gilliclement@gmail.com>
2022-02-01 16:02:27 +0100

    random

--- modules/matrice.py
+++ modules/matrice.py
@@ -1,9 +1,11 @@
 from random import randrange
 
-def random_int_list(n,bound):
+
+def random_int_list(n, bound):
     tab = []
-    for i in range(0,bound):
-        tab.append(randrange(0,bound))
+    for i in range(n):
+        tab.append(randrange(0, bound))
     return tab
+
 
 print(random_int_list(5, 10))
