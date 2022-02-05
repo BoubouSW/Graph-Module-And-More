5b80d69a410690da262a062c118ce7f587e8357b
valomay <valomay@protonmail.com>
2022-02-05 16:41:21 +0100

    remove comment

--- tests/start.py
+++ tests/start.py
@@ -5,10 +5,10 @@ import sys
 import os
 
 root = os.path.normpath(os.path.join(__file__, "./../.."))
-sys.path.append(root)  # allows us
+sys.path.append(root)
 
 from tests.open_digraph_test import *
 from tests.matrice_test import *
 
-if __name__ == "__main__":  # the following code is called only when
-    unittest.main()  # precisely this file is run0, node)
+if __name__ == "__main__":
+    unittest.main()
