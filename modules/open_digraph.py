b7da600e001dc9bb44093a3ec914a021bf957640
valomay <valomay@protonmail.com>
2022-03-15 15:55:25 +0100

    update

--- modules/open_digraph.py
+++ modules/open_digraph.py
@@ -1,14 +1,11 @@
 import numpy as np
 
 from modules.node import Node
-from modules.open_digraph_mx.open_digraph_compositions_mx import (
-    open_digrapg_compositions_mx,
-)
+from modules.open_digraph_mx.open_digraph_compositions_mx import open_digrapg_compositions_mx
 from modules.open_digraph_mx.open_digraph_get_set_mx import open_digraph_get_set_mx
 from modules.open_digraph_mx.open_digraph_display_mx import open_digraph_display_mx
-from modules.open_digraph_mx.open_digraph_constructeur_mx import (
-    open_digraph_constructeur_mx,
-)
+from modules.open_digraph_mx.open_digraph_constructeur_mx import open_digraph_constructeur_mx
+from modules.open_digraph_mx.open_digraph_predicate_mx import open_digraph_predicate_mx
 
 
 class open_digraph(
@@ -16,6 +13,7 @@ class open_digraph(
     open_digraph_get_set_mx,
     open_digraph_display_mx,
     open_digraph_constructeur_mx,
+    open_digraph_predicate_mx
 ):  # for open directed graph
     """
     data structure for a graph
@@ -89,7 +87,8 @@ class open_digraph(
                 children = node.get_children_ids
                 for id in children:
                     if not id in rm:
-                        mat[d[node.get_id], d[id]] = node.get_children_id_mult(id)
+                        mat[d[node.get_id], d[id]
+                            ] = node.get_children_id_mult(id)
         return mat
 
     def dijkstra(self, src, direction=None):
@@ -112,67 +111,3 @@ class open_digraph(
                     dist[v] = dist[u] + 1
                     prev[v] = u
         return (dist, prev)
-
-    ################
-    #   PREDICATS  #
-    ################
-
-    def is_well_formed(self) -> bool:
-        """
-        check if the graph is well formed
-        """
-        inputs = self.get_input_ids
-        outputs = self.get_output_ids
-        nodes_id = self.get_node_ids
-        for i in inputs:
-            if not (i in nodes_id):
-                return False
-            child = self.get_node_by_id(i).get_children_ids
-            if (
-                self.get_node_by_id(i).get_parent_ids != []
-                or len(child) != 1
-                or self.get_node_by_id(i).get_children_id_mult(child[0]) != 1
-            ):
-                return False
-
-        for o in outputs:
-            if not (o in nodes_id):
-                return False
-            parent = self.get_node_by_id(o).get_parent_ids
-            if (
-                self.get_node_by_id(o).get_children_ids != []
-                or len(parent) != 1
-                or self.get_node_by_id(o).get_parent_id_mult(parent[0]) != 1
-            ):
-                return False
-
-        for id in nodes_id:
-            if id != self.get_node_by_id(id).get_id:
-                return False
-
-        for node in self.get_nodes:
-            for child in node.get_children_ids:
-                mult = node.get_children_id_mult(child)
-                if self.get_node_by_id(child).get_parent_id_mult(node.get_id) != mult:
-                    return False
-            for parent in node.get_parent_ids:
-                mult = node.get_parent_id_mult(parent)
-                if (
-                    self.get_node_by_id(parent).get_children_id_mult(node.get_id)
-                    != mult
-                ):
-                    return False
-        return True
-
-    def is_cyclic(self) -> bool:
-        copy = self.copy()
-        while len(copy.nodes) != 0:
-            id = None
-            for node in copy.get_nodes:
-                if node.outdegree == 0:
-                    id = node.get_id
-            if id == None:
-                return True
-            else:
-                copy.remove_node_by_id(id)
-        return False
---
+++ modules/open_digraph_mx/open_digraph_predicate_mx.py
@@ -0,0 +1,60 @@
+class open_digraph_predicate_mx:
+    def is_well_formed(self) -> bool:
+        """
+        check if the graph is well formed
+        """
+        inputs = self.get_input_ids
+        outputs = self.get_output_ids
+        nodes_id = self.get_node_ids
+        for i in inputs:
+            if not (i in nodes_id):
+                return False
+            child = self.get_node_by_id(i).get_children_ids
+            if (
+                self.get_node_by_id(i).get_parent_ids != []
+                or len(child) != 1
+                or self.get_node_by_id(i).get_children_id_mult(child[0]) != 1
+            ):
+                return False
+
+        for o in outputs:
+            if not (o in nodes_id):
+                return False
+            parent = self.get_node_by_id(o).get_parent_ids
+            if (
+                self.get_node_by_id(o).get_children_ids != []
+                or len(parent) != 1
+                or self.get_node_by_id(o).get_parent_id_mult(parent[0]) != 1
+            ):
+                return False
+
+        for id in nodes_id:
+            if id != self.get_node_by_id(id).get_id:
+                return False
+
+        for node in self.get_nodes:
+            for child in node.get_children_ids:
+                mult = node.get_children_id_mult(child)
+                if self.get_node_by_id(child).get_parent_id_mult(node.get_id) != mult:
+                    return False
+            for parent in node.get_parent_ids:
+                mult = node.get_parent_id_mult(parent)
+                if (
+                    self.get_node_by_id(parent).get_children_id_mult(node.get_id)
+                    != mult
+                ):
+                    return False
+        return True
+
+    def is_cyclic(self) -> bool:
+        copy = self.copy()
+        while len(copy.nodes) != 0:
+            id = None
+            for node in copy.get_nodes:
+                if node.outdegree == 0:
+                    id = node.get_id
+            if id == None:
+                return True
+            else:
+                copy.remove_node_by_id(id)
+        return False
