f841b2af5d7ff7c8ae4fc8dfd611b06ea3ea7a57
valomay <valomay@protonmail.com>
2022-03-22 15:27:01 +0100

    udpate

--- tests/open_digraph_test.py
+++ tests/open_digraph_test.py
@@ -5,22 +5,6 @@ import unittest
 
 from modules.matrice import random_int_matrix
 
-
-class InitTest(unittest.TestCase):
-    def test_init_node(self):
-        n0 = Node(0, "i", {}, {1: 1})
-        self.assertEqual(n0.id, 0)
-        self.assertEqual(n0.label, "i")
-        self.assertEqual(n0.parents, {})
-        self.assertEqual(n0.children, {1: 1})
-        self.assertIsInstance(n0, Node)
-
-    def test_init_digraph(self):
-        d0 = open_digraph([], [], [])
-        self.assertEqual(d0.inputs, [])
-        self.assertEqual(d0.nodes, {})
-        self.assertEqual(d0.outputs, [])
-
 class DigraphTest(unittest.TestCase):
     def setUp(self):
         self.n0 = Node(0, "a", {3: 1, 4: 1}, {1: 1, 2: 1})
--- tests/start.py
+++ tests/start.py
@@ -5,6 +5,7 @@ import os
 root = os.path.normpath(os.path.join(__file__, "./../.."))
 sys.path.append(root)
 
+from tests.init_test import *
 from tests.open_digraph_test import *
 from tests.matrice_test import *
 from tests.node_test import *
