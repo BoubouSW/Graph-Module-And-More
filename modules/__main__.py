ee8b05b8b52669f203848ccf85906aa127b9300b
valomay <valomay@protonmail.com>
2022-04-05 17:32:49 +0200

    clean main

--- modules/__main__.py
+++ modules/__main__.py
@@ -5,18 +5,5 @@ from .utils import K_map_prop
 
 
 if __name__ == '__main__':
-
-    bc = Bool_circ.from_table('1110001000111111')
-    #print(K_map_prop('1110001000111111'))
-    
-    #Bool_circ.parse_parentheses("((x0)|(x1)(x2))|((x1)&(~(x2)))").save_as_dot_file("test.dot")
-    #bc = Bool_circ.from_table('1110001000111111')
-    print(K_map_prop('1110001000111111'))
-    Bool_circ.from_kmap("1110001000111111").save_as_dot_file("dot")
-    #bc = Bool_circ.from_table('1110001000111111')
-    #K_map_prop("1110001000111111")
-
-
-
     G = Bool_circ.random_bool_circ(6, 5, 8)
     G.display()
