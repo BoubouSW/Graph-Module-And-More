c39ea7e7de7ce0e5385df7b3ddcc37db73e58137
valomay <valomay@protonmail.com>
2022-04-12 17:12:01 +0200

    close conflict

--- modules/__main__.py
+++ modules/__main__.py
@@ -2,29 +2,6 @@ from .bool_circ import Bool_circ
 
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-    #G = Bool_circ.int_to_bites(11)
-    G = Bool_circ([], [], [])
-    n1 =G.add_node("0")
-    n2 = G.add_node('|')
-    n3 = G.add_node("1")
-    n4 = G.add_node("1")
-    n5 = G.add_node("")
-    n6 = G.add_node("&")
-
-    G.add_edge(n1, n2)
-    G.add_edge(n3, n2)
-    G.add_edge(n4, n2)
-    G.add_edge(n5, n4)
-    G.add_edge(n2, n6)
-    G.display()
-    G.or_gate(n1)
-    G.display("porte")
-    
-
-    #print(G.is_well_formed())
-    #G.save_as_dot_file("int.dot")
-=======
     G = Bool_circ.empty()
     idN = G.add_node("0")
     id1 = G.add_node("1")
@@ -37,4 +14,3 @@ if __name__ == '__main__':
     G.xor_gate(id1)
     G.save_as_dot_file("test")
 
->>>>>>> 8e34d77 (add gate)
