b1044f574c8b93d36d43d5f2e44895ab7955fb27
valomay <valomay@protonmail.com>
2022-04-19 16:21:02 +0200

    add ruls

--- modules/__main__.py
+++ modules/__main__.py
@@ -2,13 +2,5 @@ from .bool_circ import Bool_circ
 
 
 if __name__ == '__main__':
-    test = Bool_circ.empty()
-    xor1 = test.add_node("")
-    xor2 = test.add_node("^")
-    test.add_edge(xor1, xor2)
-    test.add_edge(xor1, xor2)
-    test.add_input_node(xor1)
-    test.add_input_node(xor2)
-    test.add_input_node(xor2)
-    test.xor_involution(xor1)
+    test = Bool_circ.hamming_dec()
     test.save_as_dot_file("test")
